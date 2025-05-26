ffbea560bf2b67098c3a0601369e96637e7f2d42
Izzan Nawa Syarif <izzan.nsyarif@gmail.com>
2025-05-26 17:08:41 +0700

    readmi

--- Backend/src/schema/Peminjaman.js
+++ Backend/src/schema/Peminjaman.js
@@ -5,7 +5,7 @@ const peminjamanSchema = new mongoose.Schema({
     id_buku: { type: mongoose.Schema.Types.ObjectId, ref: 'Buku' },
     tanggal_pinjam: { type: Date, default: Date.now },
     tanggal_kembali: Date,
-    status: { type: String, enum: ['dipinjam', 'dikembalikan'], default: 'dipinjam' }
+    status: { type: String, enum: [ 'requested', 'approved', 'dipinjam', 'dikembalikan'], default: 'dipinjam' }
 });
 
 module.exports = mongoose.model('Peminjaman', peminjamanSchema);
